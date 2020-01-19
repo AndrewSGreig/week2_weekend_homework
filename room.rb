cc5f36f09b28583d9f6c12b682b45cdaf639786a
User <user@users-mbp.home>
2020-01-19 16:14:03 +0000

    testing room capacity

--- room.rb
+++ room.rb
@@ -9,18 +9,17 @@ class Room
       @song_list = []
   end
 
-  def get_current_guests_count
-    return @current_guests.length
+  def get_current_guests
+    return @current_guests
   end
 
   def check_in_guest(guest)
     @current_guests << guest
   end
 
-  def check_room_capcity
-    @room.max_guests
+  def check_room_capacity
+    return @max_guests
   end
-
 
   def check_out_guest(guest)
     @current_guests.delete(guest)
@@ -32,7 +31,6 @@ class Room
 
   def add_song(song)
     @song_list << song
-    # p @song_list
   end
 
   def get_songs()
@@ -44,14 +42,14 @@ class Room
   end
 
   def check_songs(input_song)
-      song_is_present = false
-      for song in @song_list
-        if (song.song_name == input_song.song_name)
-          song_is_present = true
-        end
+    song_is_present = false
+    for song in @song_list
+      if (song.song_name == input_song.song_name)
+        song_is_present = true
       end
-      return song_is_present
     end
+    return song_is_present
+  end
 
 
 
--- specs/room_specs.rb
+++ specs/room_specs.rb
@@ -11,8 +11,13 @@ require_relative("../songs.rb")
 class TestRoom < MiniTest::Test
   def setup()
     @song1 = Song.new("purple haze")
+    @song2 = Song.new("let it be")
+    @song_list = [@song1, @song2]
     @guest1 = Guests.new("Joe Bloggs", @song1)
-    @room1 = Room.new("Lomond", 2, [@song1])
+    @guest2 = Guests.new("Fred Bloggs", @song2)
+    @guest3 = Guests.new("John Doe", @song1)
+    @total_guests = [@guest1, @guest2, @guest3]
+    @room1 = Room.new("Lomond", 2, [@song1, @song2])
     # @room2 = Room.new("Lomond", 2, 0, ["song1", "Song 2"])
 
   end
@@ -23,13 +28,15 @@ class TestRoom < MiniTest::Test
   end
 
   def test_check_in_guest()
-    assert_equal(["Joe Bloggs"], @room1.check_in_guest("Joe Bloggs"))
+    # assert_equal(["Joe Bloggs"], @room1.check_in_guest("Joe Bloggs"))
+    assert_equal([@guest1], @room1.check_in_guest(@guest1))
   end
 
   def test_check_out_guest()
-    @room1.check_in_guest("John Doe")
-    @room1.check_out_guest("John Doe")
-    assert_equal(0, @room1.get_current_guests_count)
+    @room1.check_in_guest(@guest3)
+    @room1.check_out_guest(@guest3)
+
+    assert_equal(0, @room1.get_current_guests.count)
   end
 
   def test_add_song()
@@ -38,11 +45,25 @@ class TestRoom < MiniTest::Test
   end
 
   def test_get_current_guests
-    @room1.check_in_guest(@guest)
-    assert_equal(1, @room1.get_current_guests_count)
+    @room1.check_in_guest(@guest1)
+    assert_equal(1, @room1.get_current_guests.count)
   end
 
-  # test
+  def test_room_capacity__attempted_breach()
+    #get existing # of people in room
+    # compare with room _room_capacity
+    #if spare space checkin
+    is_room_full = false
+    for guest in @total_guests
+      if @room1.get_current_guests.count < @room1.check_room_capacity
+        p "There is space in the room. Welcome #{guest.guest_name}!"
+        @room1.check_in_guest(guest)
+      else
+        is_room_full = true#
+        p "There is no more space in the room. Sorry #{guest.guest_name}"
+      end
+    end
+  end
 
 
 end
